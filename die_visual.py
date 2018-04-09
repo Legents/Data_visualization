c99abd3063a28dde45dedd451151be06970a6fb1
Legents <li_guannan@163.com>
2018-04-09 17:47:55 +0800

    六阶+十阶“
    ‘

--- die_visual.py
+++ die_visual.py
@@ -2,9 +2,9 @@ import pygal
 from die import Die
 
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 results = []
-for roll_num in range(1000):
+for roll_num in range(50000):
 	result = die_1.roll() + die_2.roll()
 	results.append(result)
 	
@@ -19,11 +19,11 @@ for value in range(2, max_result+1):
 hist = pygal.Bar()
 
 hist.title = "Results of rolling two D6 dice 1000 times."
-hist.x_labels = ['2','3','4','5','6','7','8','9','10','11','12']
+hist.x_labels = ['2','3','4','5','6','7','8','9','10','11','12','13','14','15','16']
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
-hist.add('D6 + D6', frequencies)
+hist.add('D6 + D10', frequencies)
 hist.render_to_file('die_visual.svg')	
 print(results)
 print(frequencies)
