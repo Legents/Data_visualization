1269a666851e5d8a19eda52fa6aa02803658900e
Legents <li_guannan@163.com>
2018-04-13 21:35:41 +0800

    update date

--- highs_lows.py
+++ highs_lows.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import datetime
 from matplotlib import pyplot as plt
 
 filename = 'sitka_weather_07-2014.csv'
@@ -9,17 +10,20 @@ with open(filename) as f:
 	for index,column_header in enumerate(header_row):
 		print(index, column_header)
 		
-	highs = []
+	dates, highs = [],[]
 	for row in reader:
+		current_date = datetime.strptime(row[0], "%Y-%m-%d")
+		dates.append(current_date)
 		high = int(row[1])
 		highs.append(high)
 		
 	print(highs)
 	fig = plt.figure(dpi=128, figsize=(10,6))
-	plt.plot(highs,c='red')
+	plt.plot(dates,highs,c='red')
 	
 	plt.title("Daily high temperatures,July 2014", fontsize=24)
 	plt.xlabel('',fontsize=16)
+	fig.autofmt_xdate()
 	plt.ylabel("Tempurature F",fontsize=16)
 	plt.tick_params(axis='both',which='major',labelsize=16)
 	
