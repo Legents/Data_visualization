9fe002530676b9ebf175c000bb42263dab85abf3
Legents <li_guannan@163.com>
2018-04-17 22:25:34 +0800

    more time & colored

--- highs_lows.py
+++ highs_lows.py
@@ -4,27 +4,33 @@ from matplotlib import pyplot as plt
 
 filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
-	reader = csv.reader(f)
-	header_row = next(reader)
-	
-	for index,column_header in enumerate(header_row):
-		print(index, column_header)
-		
-	dates, highs = [],[]
-	for row in reader:
-		current_date = datetime.strptime(row[0], "%Y-%m-%d")
-		dates.append(current_date)
-		high = int(row[1])
-		highs.append(high)
-		
-	print(highs)
-	fig = plt.figure(dpi=128, figsize=(10,6))
-	plt.plot(dates,highs,c='red')
-	
-	plt.title("Daily high temperatures - 2014", fontsize=24)
-	plt.xlabel('',fontsize=16)
-	fig.autofmt_xdate()
-	plt.ylabel("Tempurature F",fontsize=16)
-	plt.tick_params(axis='both',which='major',labelsize=16)
-	
-	plt.show()
+    reader = csv.reader(f)
+    header_row = next(reader)
+    
+    for index,column_header in enumerate(header_row):
+        print(index, column_header)
+    #获取日期 最高最低  
+    dates, highs, lows = [],[],[]
+    for row in reader:
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
+        
+        high = int(row[1])
+        highs.append(high)
+        
+        low = int(row[3])
+        lows.append(low)
+        
+    #绘制图形并填充
+    fig = plt.figure(dpi=128, figsize=(10,6))
+    plt.plot(dates,highs,c='red',alpha=0.5)
+    plt.plot(dates,lows,c='blue',alpha=0.5)
+    plt.fill_between(dates,highs,lows,facecolor='blue',alpha=0.1)
+    
+    plt.title("Daily high and low temperatures - 2014", fontsize=24)
+    plt.xlabel('',fontsize=16)
+    fig.autofmt_xdate()
+    plt.ylabel("Tempurature F",fontsize=16)
+    plt.tick_params(axis='both',which='major',labelsize=16)
+    
+    plt.show()
