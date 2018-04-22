be1520f08bf4abb7463a3eb98d8d5a3a466cd8e7
Legents <li_guannan@163.com>
2018-04-22 21:58:23 +0800

    updata try-except

--- highs_lows.py
+++ highs_lows.py
@@ -2,7 +2,7 @@ import csv
 from datetime import datetime
 from matplotlib import pyplot as plt
 
-filename = 'sitka_weather_2014.csv'
+filename = 'death_valley_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -12,14 +12,17 @@ with open(filename) as f:
     #获取日期 最高最低  
     dates, highs, lows = [],[],[]
     for row in reader:
-        current_date = datetime.strptime(row[0], "%Y-%m-%d")
-        dates.append(current_date)
-        
-        high = int(row[1])
-        highs.append(high)
-        
-        low = int(row[3])
-        lows.append(low)
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+            
+        except ValueError:
+            print(current_date,'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
         
     #绘制图形并填充
     fig = plt.figure(dpi=128, figsize=(10,6))
