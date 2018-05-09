dd103f23e1486f352aea1db42f6a4e6ebcd9cb0b
Legents <li_guannan@163.com>
2018-05-09 21:43:24 +0800

    wm

--- world_population.py
+++ world_population.py
@@ -1,4 +1,5 @@
 import json
+import pygal
 from country_codes import get_country_code
 
 
@@ -7,12 +8,17 @@ with open(filename) as f:
 	pop_data = json.load(f)
 	
 #打印每个国家2010的人口
+cc_populations = {}
 for pop_dict in pop_data:
 	if pop_dict['Year'] == '2010':
 		country_name = pop_dict['Country Name']
 		population = int(float(pop_dict['Value']))
 		code = get_country_code(country_name)
 		if code:
-			print(code+':'+str(population))
-		else:
-			print('ERROR - '+country_name)
+			cc_populations[code]=population
+			
+wm = pygal.Worldmap()
+wm.title = 'World Population in 2010, by Country'
+wm.add('2010',cc_populations)
+
+wm.render_to_file('world_population.svg')
