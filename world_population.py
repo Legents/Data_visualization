fa2b8a918da0bb22b2c9bba1f59de4185a220a9a
Legents <li_guannan@163.com>
2018-05-16 22:09:47 +0800

    sort

--- world_population.py
+++ world_population.py
@@ -19,23 +19,26 @@ for pop_dict in pop_data:
 			cc_populations[code]=population
 
 #根据人数将国家分组
-cc_pops_1 ,cc_pops_2, cc_pops_3 = {}, {}, {}
+cc_pops_1 ,cc_pops_2, cc_pops_3, cc_pops_4= {}, {}, {}, {}
 for cc, pop in cc_populations.items():
 	if pop < 100000000:
 		cc_pops_1[cc] = pop
-	elif pop < 1000000000:
+	elif pop < 500000000:
 		cc_pops_2[cc] = pop
-	else:
+	elif pop < 1000000000:
 		cc_pops_3[cc] = pop
+	else:
+		cc_pops_4[cc] = pop
 		
 #查看每组包含的国家数量
-print(len(cc_pops_1),len(cc_pops_2), len(cc_pops_3)) 			
+#print(len(cc_pops_1),len(cc_pops_2), len(cc_pops_3)) 			
 wm = pygal.Worldmap()
 wm_style = RotateStyle('#112233')
 wm = pygal.Worldmap(style=wm_style)
 wm.title = 'World Population in 2010, by Country'
 wm.add('0-10m',cc_pops_1)
-wm.add('10m-1bn',cc_pops_2)
-wm.add('>1bn',cc_pops_3)
+wm.add('10m-5m',cc_pops_2)
+wm.add('5m-1bn',cc_pops_3)
+wm.add('>1bn',cc_pops_4)
 
 wm.render_to_file('world_population.svg')
