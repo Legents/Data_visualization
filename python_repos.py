6b42494190a278cb35dc8ab2c581e978daa2efa0
Legents <li_guannan@163.com>
2018-05-30 21:33:09 +0800

    chart

--- python_repos.py
+++ python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS,LightenStyle as LS
 
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
@@ -10,16 +12,23 @@ print("tatal repositories:",response_dict['total_count'])
 
 #搜索有关信息
 repo_dicts = response_dict['items']
-print("repositories returned:",len(repo_dicts))
-#处理
-#print(response_dict.keys())
-#研究第一个仓库
-repo_dict = repo_dicts[0]
-print("\nselected info about first repos:")
-print('Name:',repo_dict['name'])
-print('Owner:',repo_dict['owner']['login'])
-print('Stars:',repo_dict['stargazers_count'])
-print('Repository:',repo_dict['html_url'])
-print('Creatsd:',repo_dict['created_at'])
-print('Updated:',repo_dict['updated_at'])
-print('Description:',repo_dict['description'])
+names, stars = [],[]
+for repo_dict in repo_dicts:
+	names.append(repo_dict['name'])
+	stars.append(repo_dict['stargazers_count'])
+	
+my_style = LS('#333366',base_style = LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation = 45, show_legend=False)
+chart.title = 'Most-starred Python Projects on Github'
+chart.x_labels = names
+
+chart.add('',stars)
+chart.render_to_file('python_repos.svg')
+
+print("\nSelected info about each repo:")
+for repo_dict in repo_dicts:
+	print('\nName:',repo_dict['name'])
+	print('Owner:',repo_dict['owner']['login'])
+	print('Stars:',repo_dict['stargazers_count'])
+	print('Repository:',repo_dict['html_url'])
+	print('Description:',repo_dict['description'])
